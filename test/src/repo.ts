c53646026bc01da6d29e77134bc327b6397a4bb3
Silas Davis <silas@inco.network>
2025-04-22 19:07:25 +0200

    chore: dotenv handling invariant to cwd
    Signed-off-by: Silas Davis <silas@inco.network>

--- test/src/repo.ts
+++ test/src/repo.ts
@@ -1,7 +1,13 @@
 import {readFile} from "fs/promises";
 import * as path from "path";
+import * as dotenv from "dotenv";
 
 const root = path.resolve(__dirname, '..', '..');
+const envPath = path.join(root, "/.env");
+
+export function loadDotenv() {
+  dotenv.config({ path: envPath });
+}
 
 export function resolveRoot(...paths: string[]): string {
   return path.resolve(root, ...paths);
