f1ec8dff9df1c27577263c657d8f389824fbb314
Silas Davis <silas@inco.network>
2025-04-24 13:49:15 +0200

    feat: Simplify/hide details of how network is run
    We now embed all config in the JS lib and docker containers so all that
    is required is `docker-compose up` and there are no other support files
    
    Signed-off-by: Silas Davis <silas@inco.network>

--- test/src/lightning.local.e2e.test.ts
+++ test/src/lightning.local.e2e.test.ts
@@ -1,27 +1,13 @@
 import { Lightning } from '@inco/js/lite';
-import { parseLocalEnv } from '@inco/js/local';
 import { anvil } from 'viem/chains';
 import { describe } from 'vitest';
 import { runE2ETest } from './lightning-test';
-import { loadDotEnv, readFileFromRoot } from './repo.ts';
 
 describe(`Lightning Local Node E2E`, { timeout: 50_000 }, async () => {
-  loadDotEnv();
-  const dumpParams = process.env.DUMP_ENV_FILE;
-  if (!dumpParams) {
-    throw new Error("DUMP_ENV_FILE doesn't exist!");
-  }
-  const buf = await readFileFromRoot(dumpParams);
-  const { SENDER_PRIVATE_KEY, ECIES_PUBLIC_KEY, EXECUTOR_ADDRESS } = parseLocalEnv(buf);
-  const zap = Lightning.custom({
-    executorAddress: EXECUTOR_ADDRESS,
-    eciesPublicKey: ECIES_PUBLIC_KEY,
-    chainId: BigInt(anvil.id),
-    covalidatorUrl: 'http://localhost:50055',
-  });
+  const zap = Lightning.localNode();
   runE2ETest(Math.floor(Math.random() * 100), zap, {
     chain: anvil,
-    senderPrivKey: SENDER_PRIVATE_KEY,
+    senderPrivKey: zap.deployment.senderPrivateKey,
     hostChainRpcUrl: 'http://127.0.0.1:8545',
   });
 });
