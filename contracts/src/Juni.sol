fd05b775739dd1dfe5f175ae4c25322388a215e3
hfccr <hfccr@outlook.com>
2025-09-27 03:37:52 +0530

    Add basic structure for fhe hook

---
+++ contracts/src/Juni.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {console2} from "forge-std/console2.sol";
+import {BaseHook} from "@openzeppelin/uniswap-hooks/src/base/BaseHook.sol";
+import {IPoolManager, SwapParams} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {CurrencySettler} from "@openzeppelin/uniswap-hooks/src/utils/CurrencySettler.sol";
+import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary, toBeforeSwapDelta} from "@uniswap/v4-core/src/types/BeforeSwapDelta.sol";
+import {IUniswapV4Router04} from "hookmate/interfaces/router/IUniswapV4Router04.sol";
+import {BalanceDelta} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/src/types/PoolId.sol";
+import {ebool, e, euint256} from "@inco/lightning/src/Lib.sol";
+
+contract Juni is BaseHook {
+    using PoolIdLibrary for PoolKey;
+    using CurrencySettler for Currency;
+    using e for euint256;
+    using e for ebool;
+    using e for bool;
+    using e for bytes;
+    using e for address;
+    using e for uint256;
+
+    bool public initialized;
+    PoolKey public poolKey;
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function _beforeInitialize(address, PoolKey calldata _poolKey, uint160) internal override returns (bytes4) {
+        // require that pool key should not be already set
+        require(!initialized, "Only one pool can be initialized");
+        poolKey = _poolKey;
+        initialized = true;
+        return this.beforeInitialize.selector;
+    }
+
+    function _beforeSwap(
+        address,
+        PoolKey calldata key,
+        SwapParams calldata params,
+        bytes calldata
+    ) internal override returns (bytes4, BeforeSwapDelta, uint24) {
+        console2.log("before swap called");
+        return (this.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+    }
+
+    function _afterSwap(
+        address,
+        PoolKey calldata,
+        SwapParams calldata,
+        BalanceDelta,
+        bytes calldata
+    ) internal override returns (bytes4, int128) {
+        console2.log("after swap called");
+        return (this.afterSwap.selector, 0);
+    }
+
+    function addESwap() public returns (bool) {
+        // todo: create a function to add an encrypted swap
+        return true;
+    }
+
+    function requestDecryptionForBlock(uint256 blockNumber) public returns (bool) {
+        // todo: request decryption for a block
+        return true;
+    }
+
+    function onDecryptionCallback(uint256, bytes32 decryptedBlock, bytes memory data) external {
+        // todo: create a function to handle decrypted state callback for a block
+    }
+
+    function runESwap(uint256 decryptedBlock, uint256 i) public returns (bool) {
+        // todo: create a function to run a single encrypted block
+    }
+
+    function runESwaps() public returns (bool) {
+        // todo: create function to run encrypted swaps for a block
+        return true;
+    }
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return
+            Hooks.Permissions({
+                beforeInitialize: true,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                afterAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: true,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+}
---
+++ contracts/src/test/Juni.t.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+
+import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {BalanceDelta} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/src/types/PoolId.sol";
+import {CurrencyLibrary, Currency} from "@uniswap/v4-core/src/types/Currency.sol";
+import {StateLibrary} from "@uniswap/v4-core/src/libraries/StateLibrary.sol";
+import {LiquidityAmounts} from "@uniswap/v4-core/test/utils/LiquidityAmounts.sol";
+import {IPositionManager} from "@uniswap/v4-periphery/src/interfaces/IPositionManager.sol";
+import {Constants} from "@uniswap/v4-core/test/utils/Constants.sol";
+import {SwapParams} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ebool, e, euint256} from "@inco/lightning/src/Lib.sol";
+
+import {IncoTest} from "@inco/lightning/src/test/IncoTest.sol";
+import {GWEI} from "@inco/shared/src/TypeUtils.sol";
+
+import {EasyPosm} from "./utils/libraries/EasyPosm.sol";
+import {Deployers} from "./utils/Deployers.sol";
+import {Juni} from "../Juni.sol";
+import {console2} from "forge-std/console2.sol";
+
+contract JuniTest is IncoTest, Deployers {
+    using EasyPosm for IPositionManager;
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+    using StateLibrary for IPoolManager;
+    using e for bool;
+    using e for ebool;
+    using e for euint256;
+    using e for uint256;
+
+    Currency currency0;
+    Currency currency1;
+    PoolKey poolKey;
+
+    Juni hook;
+    PoolId poolId;
+
+    uint256 tokenId;
+    uint256 tokenId1;
+    int24 tickLower;
+    int24 tickUpper;
+
+    function setUp() public override {
+        super.setUp();
+        // Deploys all required artifacts.
+        deployArtifacts();
+
+        (currency0, currency1) = deployCurrencyPair();
+
+        // Deploy the hook to an address with the correct flags
+        address flags = address(
+            uint160(Hooks.BEFORE_SWAP_FLAG | Hooks.BEFORE_INITIALIZE_FLAG | Hooks.AFTER_SWAP_FLAG) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
+        );
+        bytes memory constructorArgs = abi.encode(poolManager); // Add all the necessary constructor arguments from the hook
+        deployCodeTo("Juni.sol:Juni", constructorArgs, flags);
+        hook = Juni(flags);
+
+        // Create the pool
+        poolKey = PoolKey(currency0, currency1, 3000, 60, IHooks(hook));
+        poolId = poolKey.toId();
+        poolManager.initialize(poolKey, Constants.SQRT_PRICE_1_1);
+
+        // Provide full-range liquidity to the pool
+        tickLower = TickMath.minUsableTick(poolKey.tickSpacing);
+        tickUpper = TickMath.maxUsableTick(poolKey.tickSpacing);
+
+        uint128 liquidityAmount = 100e18;
+
+        (uint256 amount0Expected, uint256 amount1Expected) = LiquidityAmounts.getAmountsForLiquidity(
+            Constants.SQRT_PRICE_1_1,
+            TickMath.getSqrtPriceAtTick(tickLower),
+            TickMath.getSqrtPriceAtTick(tickUpper),
+            liquidityAmount
+        );
+
+        (tokenId, ) = positionManager.mint(
+            poolKey,
+            tickLower,
+            tickUpper,
+            liquidityAmount,
+            amount0Expected + 1,
+            amount1Expected + 1,
+            address(this),
+            block.timestamp,
+            Constants.ZERO_BYTES
+        );
+        (tokenId1, ) = positionManager.mint(
+            poolKey,
+            tickLower,
+            tickUpper,
+            liquidityAmount,
+            amount0Expected + 1,
+            amount1Expected + 1,
+            address(hook),
+            block.timestamp,
+            Constants.ZERO_BYTES
+        );
+    }
+
+    function testBasicSwap() public {
+        // Perform a test swap //
+        uint256 amountIn = 1e18;
+        BalanceDelta swapDelta = swapRouter.swapExactTokensForTokens({
+            amountIn: amountIn,
+            amountOutMin: 0, // Very bad, but we want to allow for unlimited price impact
+            zeroForOne: true,
+            poolKey: poolKey,
+            hookData: Constants.ZERO_BYTES,
+            receiver: address(this),
+            deadline: block.timestamp + 1
+        });
+
+        assertEq(int256(swapDelta.amount0()), -int256(amountIn));
+    }
+}
